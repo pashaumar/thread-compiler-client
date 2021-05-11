594a34d75f055a5cc25bac2826d547527c38313f
pashaumar <pasha.u.144@gmail.com>
2021-05-11 14:55:10 +0530

    deleting unnecsessary data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,3 @@
-import Link from "next/link";
-import Layout from "../components/Layout";
-
-const IndexPage = () => <div>welcome to next js with typescript</div>;
+const IndexPage = () => <div>welcome to next js with typescript </div>;
 
 export default IndexPage;
